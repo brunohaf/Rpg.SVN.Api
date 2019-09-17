4688caf4c7c67a62702ccba381979f1cf5613345
Bruno H. A. Fonseca <bruno3145@gmail.com>
2019-09-17 10:33:46 -0300

    Scraper structure implemented

---
+++ Rpg.Svn.Api/Interfaces/IScraperService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.Svn.Api.Interfaces
+{
+    public interface IScraperService
+    {
+        void Test(string login, string senha);
+    }
+}
